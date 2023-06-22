32156207a2b74a5e773d8cc8dcf4c97121a3823c
prasadbhavsar28 <54167302+prasadbhavsar28@users.noreply.github.com>
2023-06-22 22:35:06 +0530

    Update Main.java (#2)
    Added more changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! - Learning ");
+        System.out.println("Hello world! - Learning-123 ");
         // Checkin from bhavsarprasad28 
     }
 }
