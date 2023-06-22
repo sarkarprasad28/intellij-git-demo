7e502dbc848ae4ac8b7f111353b4b38da2fcf1a1
sarkarprasad28 <112084238+sarkarprasad28@users.noreply.github.com>
2023-06-22 23:22:43 +0530

    new line added (#3)

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -2,6 +2,7 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("123");
+        System.out.println("123");
         // Prasad added this line
     }
 }
