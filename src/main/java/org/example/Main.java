4271b19cb362a8a85ea4f7a92a94822cdf81d629
prasadbhavsar28 <54167302+prasadbhavsar28@users.noreply.github.com>
2023-06-22 21:59:44 +0530

    Added dash in place of plus
    Added dash in place of plus

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! + Learning ");
+        System.out.println("Hello world! - Learning ");
         // Checkin from bhavsarprasad28 
     }
 }
